cda2e92742902a46853a32a606daff1a0f0a3af4
Jorge <jorgeseg1900@gmail.com>
2023-05-22 01:07:59 -0600

    actualizado a la 1 am

---
+++ views/reparaciones/mostrar.php
@@ -0,0 +1,227 @@
+<?php
+
+include('../../app/config.php');
+include('../../layout/sesion.php');
+include('../../layout/part1.php');
+
+include('../../app/controllers/reparaciones/consulta_clientes.php');
+
+?>
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">FORMULARIO PARA AGREGAR CLIENTES</h1>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <div class="content">
+        <div class="container-fluid">
+
+            <div class="row">
+                <div class="col-md-9">
+                    <div class="card card-primary" style="transition: all 0.15s ease 0s; height: inherit; width: inherit;">
+                        <div class="card-header">
+                            <h3 class="card-title">Rellene los datos correctamente...</h3>
+
+                            <div class="card-tools">
+                                <button type="button" class="btn btn-tool" data-card-widget="maximize"><i class="fas fa-expand"></i>
+                                </button>
+                            </div>
+                            <!-- /.card-tools -->
+                        </div>
+                        <!-- /.card-header -->
+                        <div class="card-body" style="display: block;">
+                            <div class="row">
+                                <h5> Datos de clientes </h5>
+                                <div style="width: 20px"></div>
+                                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal-buscar_cliente">
+                                    <i class="fa fa-search"></i>
+                                    Buscar
+                                </button>
+                            </div>
+                            <hr>
+
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <form action="../../app/controllers/reparaciones/crear_reparaciones.php" method="post" enctype="multipart/form-data">
+                                        <div class="row">
+                                            <div class="col-md-4">
+                                                <div class="form-group">
+                                                    <input type="text" name="id_clientes" id="cliente_id" hidden>
+                                                    <label for="">Cliente:</label>
+                                                    <input type="text" nombre="cliente" class="form-control" value="<?php echo $nombre?> <?php echo $apellido?>" disabled>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-4">
+                                                <div class="form-group">
+                                                    <label for="">Marca del Vehiculo:</label>
+                                                    <div style="display: flex">
+                                                        <input type="text" nombre="marca" class="form-control" value="<?php echo $marcavehiculo?>" disabled>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-4">
+                                                <div class="form-group">
+                                                    <label for="">Modelo del Vehiculo:</label>
+                                                    <input type="text" name="modelo" id="modelo" class="form-control" disabled>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-md-4">
+                                                <div class="form-group">
+                                                    <input type="text" id="id_producto" hidden>
+                                                    <label for="">Placa del Vehiculo:</label>
+                                                    <input type="text" nombre="placa" class="form-control" id="placa" disabled>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-4">
+                                                <div class="form-group">
+                                                    <label for="">Fecha:</label>
+                                                    <input type="date" class="form-control" name="fecha">
+                                                </div>
+                                            </div>
+                                            <div class="col-md-3">
+                                                <div class="form-group">
+                                                    <label for="">Imagen de la falla:</label>
+                                                    <input type="file" name="image" class="form-control" id="file">
+                                                    <br>
+                                                    <output id="list"></output>
+                                                    <script>
+                                                        function archivo(evt) {
+                                                            var files = evt.target.files; // FileList object
+                                                            // Obtenemos la imagen del campo "file".
+                                                            for (var i = 0, f; f = files[i]; i++) {
+                                                                //Solo admitimos imágenes.
+                                                                if (!f.type.match('image.*')) {
+                                                                    continue;
+                                                                }
+                                                                var reader = new FileReader();
+                                                                reader.onload = (function(theFile) {
+                                                                    return function(e) {
+                                                                        // Insertamos la imagen
+                                                                        document.getElementById("list").innerHTML = ['<img class="thumb thumbnail" src="', e.target.result, '" width="100%" title="', escape(theFile.name), '"/>'].join('');
+                                                                    };
+                                                                })(f);
+                                                                reader.readAsDataURL(f);
+                                                            }
+                                                        }
+                                                        document.getElementById('file').addEventListener('change', archivo, false);
+                                                    </script>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-md-8">
+                                                <div class="form-group">
+                                                    <label for="">Descripción de la falla:</label>
+                                                    <textarea name="falla" cols="30" rows="3" class="form-control" placeholder="Escriba la descripcion de la falla"></textarea>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-4">
+                                                <div class="form-group">
+                                                    <select name="estado" id="" class="form-control" hidden>
+                                                        <?php
+                                                        foreach ($datos_estado as $dato_estado) { ?>
+                                                            <option value="<?php echo $dato_estado['id_est']; ?>"><?php echo $dato_estado['estado']; ?></option>
+                                                        <?php
+                                                        }
+                                                        ?>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <hr>
+                                        <div class="form-group">
+                                            <button type="submit" class="btn btn-primary">Guardar</button>
+                                            <a href="reparaciones.php" class="btn btn-secondary">Cancelar</a>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /.card-body -->
+                </div>
+            </div>
+        </div>
+        <!-- /.row -->
+    </div>
+</div><!-- /.container-fluid -->
+<!-- /.content -->
+<!-- /.content-wrapper -->
+<?php
+
+include('../../layout/mensaje.php');
+include('../../layout/part2.php');
+
+?>
+
+;<script>
+    $(function() {
+        $("#example1").DataTable({
+            "pageLength": 5,
+            "language": {
+                "emptyTable": "No hay información",
+                "info": "Mostrando _START_ a _END_ de _TOTAL_ Productos",
+                "infoEmpty": "Mostrando 0 a 0 de 0 Productos",
+                "infoFiltered": "(Filtrado de _MAX_ total Productos)",
+                "infoPostFix": "",
+                "thousands": ",",
+                "lengthMenu": "Mostrar _MENU_ Productos",
+                "loadingRecords": "Cargando...",
+                "processing": "Procesando...",
+                "search": "Buscador:",
+                "zeroRecords": "Sin resultados encontrados",
+                "paginate": {
+                    "first": "Primero",
+                    "last": "Ultimo",
+                    "next": "Siguiente",
+                    "previous": "Anterior"
+                }
+            },
+            "responsive": true,
+            "lengthChange": true,
+            "autoWidth": false,
+
+        }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+    });
+
+
+    $(function() {
+        $("#example2").DataTable({
+            "pageLength": 5,
+            "language": {
+                "emptyTable": "No hay información",
+                "info": "Mostrando _START_ a _END_ de _TOTAL_ Proveedores",
+                "infoEmpty": "Mostrando 0 a 0 de 0 Proveedores",
+                "infoFiltered": "(Filtrado de _MAX_ total Proveedores)",
+                "infoPostFix": "",
+                "thousands": ",",
+                "lengthMenu": "Mostrar _MENU_ Proveedores",
+                "loadingRecords": "Cargando...",
+                "processing": "Procesando...",
+                "search": "Buscador:",
+                "zeroRecords": "Sin resultados encontrados",
+                "paginate": {
+                    "first": "Primero",
+                    "last": "Ultimo",
+                    "next": "Siguiente",
+                    "previous": "Anterior"
+                }
+            },
+            "responsive": true,
+            "lengthChange": true,
+            "autoWidth": false,
+
+        }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+    });
+</script>
--- views/reparaciones/reparaciones.php
+++ views/reparaciones/reparaciones.php
@@ -1,157 +1,164 @@
 <?php
 
-include ('../../app/config.php');
-include ('../../layout/sesion.php');
-include ('../../layout/part1.php');
-include ('../../app/controllers/reparaciones/listado_reparaciones.php');
+include('../../app/config.php');
+include('../../layout/sesion.php');
+include('../../layout/part1.php');
+include('../../app/controllers/reparaciones/listado_reparaciones.php');
 
 ?>
 
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0">LISTADO DE REPARACIONES</h1>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
-
-    <!-- Main content -->
-    <div class="content">
-      <div class="container-fluid">
-
-      <a href="crear.php" class="btn btn-primary"><h5>Nueva Reparacion</h5></a>
-      <hr>
-            <div class="card card-outline card-primary" style="transition: all 0.15s ease 0s; height: inherit; width: inherit;">
-              <div class="card-header">
-                <h3 class="card-title">Aqui se muestran los datos de los reparaciones...</h3>
-
-                <div class="card-tools">
-                  <button type="button" class="btn btn-tool" data-card-widget="maximize"><i class="fas fa-expand"></i>
-                  </button>
-                </div>
-                <!-- /.card-tools -->
-              </div>
-              <!-- /.card-header -->
-              <div class="card-body" style="display: block;">
-                <table id="example1" class="table table-bordered table-hover">
-                  <thead>
-                  <tr>
-                    <th>Cliente</th>
-                    <th>Marca Vehiculo</th>
-                    <th>Modelo Vehiculo</th>
-                    <th>Placas</th>
-                    <th>Fallas</th>
-                    <th>Fecha Ingreso</th>
-                    <th>Imagen</th>
-                    <th>Acciones</th>
-                  </tr>
-                  </thead>
-                  <tbody>
-                    <?php
-                    $contador = 0;
-                    foreach($datos_reparaciones as $dato_reparaciones){ 
-                      $reparaciones_id = $dato_reparaciones['id_rep']; ?>
-                    <tr>
-                      
-                      <td><?php echo $dato_reparaciones['nombre'];?>  <?php echo $dato_reparaciones['apellido'];?></td>
-                      <td><?php echo $dato_reparaciones['marca_vehiculo'];?></td>
-                      <td><?php echo $dato_reparaciones['modelo_vehiculo'];?></td>
-                      <td><?php echo $dato_reparaciones['placa_vehiculo'];?></td>
-                      <td><?php echo $dato_reparaciones['fallas'];?></td>
-                      <td><?php echo $dato_reparaciones['fecha_ingreso'];?></td>
-                      <td><?php echo $dato_reparaciones['imagen'];?></td>
-                      <td>
-                        <center>
-                        <div class="btn-group">
-                          <a href="mostrar.php?id=<?php echo $reparaciones_id; ?>" type="button" class="btn btn-warning"><i class="fa fa-eye"></i></a>
-                          <a href="modificar.php?id=<?php echo $reparaciones_id; ?>" type="button" class="btn btn-success"><i class="fa fa-pencil-alt"></i> </a>
-                          <a href="eliminar.php?id=<?php echo $reparaciones_id; ?>" type="button" class="btn btn-danger"><i class="fa fa-trash"></i></a>
-                        </div>
-                        <a href="generar_factura.php" type="button" class="btn btn-primary"><i class="fa-sharp fa-solid fa-cash-register"></i> Generar Factura</a>
-                        
-                        </center>
-                      </td>
-                    </tr>
-
-                    <?php
-
-                    }
-
-                    ?>
-                  </tbody>
-                  
-                </table>
-              </div>
-              <!-- /.card-body -->
-            </div>
-        <!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content -->
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <div class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1 class="m-0">LISTADO DE REPARACIONES</h1>
+        </div><!-- /.col -->
+      </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
   </div>
-  <!-- /.content-wrapper -->
+  <!-- /.content-header -->
+
+  <!-- Main content -->
+  <div class="content">
+    <div class="container-fluid">
+
+      <a href="crear.php" class="btn btn-primary">
+        <h5>Nueva Reparacion</h5>
+      </a>
+      <hr>
+      <div class="card card-outline card-primary" style="transition: all 0.15s ease 0s; height: inherit; width: inherit;">
+        <div class="card-header">
+          <h3 class="card-title">Aqui se muestran los datos de los reparaciones...</h3>
+
+          <div class="card-tools">
+            <button type="button" class="btn btn-tool" data-card-widget="maximize"><i class="fas fa-expand"></i>
+            </button>
+          </div>
+          <!-- /.card-tools -->
+        </div>
+        <!-- /.card-header -->
+        <div class="card-body" style="display: block;">
+          <table id="example1" class="table table-bordered table-hover">
+            <thead>
+              <tr>
+                <th>Cliente</th>
+                <th>Marca Vehiculo</th>
+                <th>Modelo Vehiculo</th>
+                <th>Placas</th>
+                <th>Fallas</th>
+                <th>Fecha Ingreso</th>
+                <th>Imagen</th>
+                <th>Estado</th>
+                <th>Acciones</th>
+              </tr>
+            </thead>
+            <tbody>
+              <?php
+              $contador = 0;
+              foreach ($datos_reparaciones as $dato_reparaciones) {
+                $reparaciones_id = $dato_reparaciones['id_rep']; ?>
+                <tr>
+
+                  <td><?php echo $dato_reparaciones['nombre']; ?> <?php echo $dato_reparaciones['apellido']; ?></td>
+                  <td><?php echo $dato_reparaciones['marca_vehiculo']; ?></td>
+                  <td><?php echo $dato_reparaciones['modelo_vehiculo']; ?></td>
+                  <td><?php echo $dato_reparaciones['placa_vehiculo']; ?></td>
+                  <td><?php echo $dato_reparaciones['fallas']; ?></td>
+                  <td><?php echo $dato_reparaciones['fecha_ingreso']; ?></td>
+                  <td>
+                    <img src="<?php echo $URL."/views/reparaciones/img_reparaciones/".$dato_reparaciones['imagen']; ?>" width="150px" alt="asdf">
+                  </td>
+                  <td><?php echo $dato_reparaciones['estado'];?></td>
+                  <td>
+                    <center>
+                      <div class="btn-group">
+                        <a href="mostrar.php?id=<?php echo $reparaciones_id; ?>" type="button" class="btn btn-warning"><i class="fa fa-eye"></i></a>
+                        <a href="modificar.php?id=<?php echo $reparaciones_id; ?>" type="button" class="btn btn-success"><i class="fa fa-pencil-alt"></i> </a>
+                        <a href="eliminar.php?id=<?php echo $reparaciones_id; ?>" type="button" class="btn btn-danger"><i class="fa fa-trash"></i></a>
+                      </div>
+                      <hr>
+                      <a href="generar_factura.php" type="button" class="btn btn-primary"><i class="fa-sharp fa-solid fa-cash-register"></i> Generar Factura</a>
+
+                    </center>
+                  </td>
+                </tr>
+
+              <?php
+
+              }
+
+              ?>
+            </tbody>
+
+          </table>
+        </div>
+        <!-- /.card-body -->
+      </div>
+      <!-- /.row -->
+    </div><!-- /.container-fluid -->
+  </div>
+  <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
 <?php
 
-include ('../../layout/mensaje.php');
-include ('../../layout/part2.php');?>
+include('../../layout/mensaje.php');
+include('../../layout/part2.php'); ?>
 
 <script>
-  $(function () {
+  $(function() {
     $("#example1").DataTable({
       "pageLength": 5,
       "language": {
-              "emptyTable": "No hay información",
-              "decimal": "",
-              "info": "Mostrando _START_ a _END_ de _TOTAL_ reparaciones",
-              "infoEmpty": "Mostrando 0 to 0 of 0 reparaciones",
-              "infoFiltered": "(Filtrado de _MAX_ total reparaciones)",
-              "infoPostFix": "",
-              "thousands": ",",
-              "lengthMenu": "Mostrar _MENU_ reparaciones",
-              "loadingRecords": "Cargando...",
-              "processing": "Procesando...",
-              "search": "Buscador:",
-              "zeroRecords": "Sin resultados encontrados",
-              "paginate": {
-                  "first": "Primero",
-                  "last": "Ultimo",
-                  "next": "Siguiente",
-                  "previous": "Anterior"
-              }
-            },
-      "responsive": true, "lengthChange": true, "autoWidth": false,
-      buttons:  [{
-			extend: 'collection',
-			text: 'Reportes',
-			orientation: 'landscape',
-			buttons: [{
-				text: 'Copiar',
-				extend: 'copy'
-			}, {
-				extend: 'pdf'
-			}, {
-				extend: 'csv'
-			}, {
-				extend: 'excel'
-			}, {
-				text: 'Imprimir',
-				extend: 'print'
-			}
-			]
-		},
-			{
-				extend: 'colvis',
-				text: 'Mostrar / Ocultar columnas',
-				collectionLayout: 'fixed three-column'
-			}
-		],
+        "emptyTable": "No hay información",
+        "decimal": "",
+        "info": "Mostrando _START_ a _END_ de _TOTAL_ reparaciones",
+        "infoEmpty": "Mostrando 0 to 0 of 0 reparaciones",
+        "infoFiltered": "(Filtrado de _MAX_ total reparaciones)",
+        "infoPostFix": "",
+        "thousands": ",",
+        "lengthMenu": "Mostrar _MENU_ reparaciones",
+        "loadingRecords": "Cargando...",
+        "processing": "Procesando...",
+        "search": "Buscador:",
+        "zeroRecords": "Sin resultados encontrados",
+        "paginate": {
+          "first": "Primero",
+          "last": "Ultimo",
+          "next": "Siguiente",
+          "previous": "Anterior"
+        }
+      },
+      "responsive": true,
+      "lengthChange": true,
+      "autoWidth": false,
+      buttons: [{
+          extend: 'collection',
+          text: 'Reportes',
+          orientation: 'landscape',
+          buttons: [{
+            text: 'Copiar',
+            extend: 'copy'
+          }, {
+            extend: 'pdf'
+          }, {
+            extend: 'csv'
+          }, {
+            extend: 'excel'
+          }, {
+            text: 'Imprimir',
+            extend: 'print'
+          }]
+        },
+        {
+          extend: 'colvis',
+          text: 'Mostrar / Ocultar columnas',
+          collectionLayout: 'fixed three-column'
+        }
+      ],
     }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
   });
 </script>
-
