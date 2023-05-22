2dc179b5cb7143fcba8393b31c1ce7623b842813
JRG-23SV <jorgeseg1900@gmail.com>
2023-05-22 07:49:43 -0600

    actualizado a las 7:50 am

--- views/usuarios/crear.php
+++ views/usuarios/crear.php
@@ -14,7 +14,7 @@ include ('../../app/controllers/usuarios/consulta_roles.php');
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0">CREACION DE USUARIOS</h1>
+            <h1 class="m-0">CREACION DE USUARIOSS</h1>
           </div><!-- /.col -->
         </div><!-- /.row -->
       </div><!-- /.container-fluid -->
